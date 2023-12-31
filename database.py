ca89f8872780e9db1376e3d66ed3efc97213cb67
Lukealveshow <92194606+Lukealveshow@users.noreply.github.com>
2023-12-31 20:30:51 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,20 +1,23 @@
 import streamlit as st
 from pymongo import MongoClient
-import pandas as pd
+import ssl
 
-# Função para inserir dados no banco de dados
+# Criação do contexto SSL personalizado
+ssl_ctx = ssl.create_default_context()
+ssl_ctx.check_hostname = False
+ssl_ctx.verify_mode = ssl.CERT_NONE
 def insert_data_mongodb(name, age, gender, text_summarization, summarized_text, text_generation,
                 question, answer, text_translation, language, translated_text):
     try:
-        # Obtenha a URL de conexão do arquivo secrets.toml
-        mongo_url = st.secrets["mongo"]["mongodb+srv://Lucas:Lucas1717@cluster0.jm1yedl.mongodb.net/?retryWrites=true&w=majority"]
+        ssl_ctx = ssl.create_default_context()
+        ssl_ctx.check_hostname = False
+        ssl_ctx.verify_mode = ssl.CERT_NONE
 
-        # Conecte-se ao banco de dados MongoDB usando pymongo
-        client = MongoClient(mongo_url)
-        db = client.app_dados
-        data_collection = db.data
+        client = MongoClient("mongodb+srv://Lucas:Lucas1717@cluster0.jm1yedl.mongodb.net/?retryWrites=true&w=majority", ssl_context=ssl_ctx)
+  
+        db = client.data  # Nome do banco de dados
+        data_collection = db.app_dados  # Nome da coleção
 
-        # Cria um documento com os dados
         data = {
             'name': name,
             'age': age,
@@ -29,10 +32,9 @@ def insert_data_mongodb(name, age, gender, text_summarization, summarized_text, 
             'translated_text': translated_text
         }
 
-        # Insira o documento na coleção
         data_collection.insert_one(data)
 
         st.success("Dados inseridos com sucesso!")
-
+    
     except Exception as e:
         st.error(f"Erro ao inserir dados: {e}")
