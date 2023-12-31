c8782c0de310e19c42e711de2e4cf0c4fc27d28d
Lucas Alves Martins <lucasalvesmartins10@gmail.com>
2023-12-31 18:08:08 -0300

    codigos com mongodb

--- app.py
+++ app.py
@@ -2,33 +2,40 @@ import streamlit as st
 from deep_translator import GoogleTranslator
 from summarization import summarize_text
 from generation import generate_answer
-from database import insert_data
+from database import insert_data_mongodb
 
 def translate_page(language):
     # Using deep_translator for page translation
     translator = GoogleTranslator(source='auto', target=language)
     translated_title = translator.translate("Seja Bem Vindo ao MAKENLP")
     st.title(translated_title)
+    
     # Additional fields
     translated_name = translator.translate("Nome:")
     name = st.text_input(translated_name)
+    
     translated_age = translator.translate("Idade:")
     age = st.number_input(translated_age, step=1)
+    
     translated_gender = translator.translate("Gênero:")
     gender_options = [translator.translate('Masculino'), translator.translate('Feminino')]
     gender = st.selectbox(translated_gender, options=gender_options)
+    
     # Section for text input
     translated_text_input = translator.translate("Digite o texto para resumir:")
     st.subheader(translated_text_input)
     text_summarization = st.text_area("", height=150)
+    
     # Button to summarize text
     if st.button(translator.translate("Resumir Texto")):
         summarized_text = summarize_text(text_summarization)
         st.subheader(translator.translate("Texto Resumido"))
         st.write(summarized_text)
+    
     translated_text_gen = translator.translate("Digite o texto:")
     st.subheader(translated_text_gen)
     text_generation = st.text_area("", height=150, key='text_generation')
+    
     translated_question = translator.translate("Faça uma pergunta sobre o texto:")
     question = st.text_input(translated_question, key='question')
 
@@ -42,6 +49,7 @@ def translate_page(language):
     translated_text_trans = translator.translate("Digite o texto para traduzir:")
     st.subheader(translated_text_trans)
     text_translation = st.text_area("", height=150, key='text_translation')
+    
     translated_lang = translator.translate("Selecione o idioma de destino:")
     language_options = ['en', 'es', 'fr', 'pt']
     language = st.selectbox(translated_lang, options=language_options, key='language')
@@ -58,9 +66,9 @@ def translate_page(language):
         answer = generate_answer(question, text_generation)
         translated_text = GoogleTranslator(source='auto', target=language).translate(text_translation)
 
-        # Utilize a função insert_data do database.py
-        insert_data(name, age, gender, text_summarization, summarized_text, text_generation,
-                    question, answer, text_translation, language, translated_text)
+        # Utilize a função insert_data_mongodb do database.py
+        insert_data_mongodb(name, age, gender, text_summarization, summarized_text, text_generation,
+                            question, answer, text_translation, language, translated_text)
         st.success(translator.translate("Dados inseridos com sucesso!"))
 
 # Run the Streamlit app
--- database.py
+++ database.py
@@ -1,35 +1,33 @@
 import streamlit as st
-import pandas as pd
+from pymongo import MongoClient
 
-# Função para inserir dados no banco de dados
 def insert_data(name, age, gender, text_summarization, summarized_text, text_generation,
                 question, answer, text_translation, language, translated_text):
     try:
-        # Inicializa a conexão com o banco de dados
-        conn = st.connection('mysql', type='sql')
-        
-        # Cria um DataFrame com os dados
+
+        client = MongoClient("mongodb+srv://Lucas:Lucas1717@cluster0.jm1yedl.mongodb.net/?retryWrites=true&w=majority")
+  
+        db = client.app_dados
+
+        data_collection = db.data
+
         data = {
-            'name': [name],
-            'age': [age],
-            'gender': [gender],
-            'text_summarization': [text_summarization],
-            'summarized_text': [summarized_text],
-            'text_generation': [text_generation],
-            'question': [question],
-            'answer': [answer],
-            'text_translation': [text_translation],
-            'language': [language],
-            'translated_text': [translated_text]
+            'name': name,
+            'age': age,
+            'gender': gender,
+            'text_summarization': text_summarization,
+            'summarized_text': summarized_text,
+            'text_generation': text_generation,
+            'question': question,
+            'answer': answer,
+            'text_translation': text_translation,
+            'language': language,
+            'translated_text': translated_text
         }
 
-        df = pd.DataFrame(data)
-
-        # Executa a inserção no banco de dados
-        conn.insert('mytable', df)
+        data_collection.insert_one(data)
 
         st.success("Dados inseridos com sucesso!")
     
     except Exception as e:
         st.error(f"Erro ao inserir dados: {e}")
-
