9f14be48e1022ea37dca13c16203373f5ff8b8f2
Lukealveshow <92194606+Lukealveshow@users.noreply.github.com>
2023-12-31 19:19:32 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,20 +1,16 @@
 import streamlit as st
 from pymongo import MongoClient
-import pandas as pd
 
-# Função para inserir dados no banco de dados
 def insert_data_mongodb(name, age, gender, text_summarization, summarized_text, text_generation,
                 question, answer, text_translation, language, translated_text):
     try:
-        # Obtenha a URL de conexão do arquivo secrets.toml
-        mongo_url = st.secrets["mongo"]["mongodb+srv://Lucas:Lucas1717@cluster0.jm1yedl.mongodb.net/?retryWrites=true&w=majority"]
 
-        # Conecte-se ao banco de dados MongoDB usando pymongo
-        client = MongoClient(mongo_url)
+        client = MongoClient("mongodb+srv://Lucas:Lucas1717@cluster0.jm1yedl.mongodb.net/?retryWrites=true&w=majority")
+  
         db = client.app_dados
+
         data_collection = db.data
 
-        # Cria um documento com os dados
         data = {
             'name': name,
             'age': age,
@@ -29,10 +25,9 @@ def insert_data_mongodb(name, age, gender, text_summarization, summarized_text, 
             'translated_text': translated_text
         }
 
-        # Insira o documento na coleção
         data_collection.insert_one(data)
 
         st.success("Dados inseridos com sucesso!")
-
+    
     except Exception as e:
         st.error(f"Erro ao inserir dados: {e}")
